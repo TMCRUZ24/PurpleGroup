e97320b2619e23852e20bda34126bd13daa6b971
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-25 19:38:01 -0700

    week 5

---
+++ dbh.inc.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Purple Group Project v1.5
+Module v5.0
+
+Programers:
+Tabitha Binkley
+Tyson Cruz
+Matthew McSpadden
+
+last updated 11/25/2018
+
+Module 5.0 adds the search ability for blogs posts. 
+
+This file connects to the server and the database we are using.
+*/
+
+
+
+$conn = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
