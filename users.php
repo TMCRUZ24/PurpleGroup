fb8ca08f8c52e62387ceaa6317968099a0a83669
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-12-02 22:00:58 -0700

    week 6

--- users.php
+++ users.php
@@ -1,17 +1,17 @@
 <?php
 
 /*
-Purple Group Project v1.5
-Module v5.0
+Purple Group Project v1.6
+Module v6.0
 
 Programers:
 Tabitha Binkley
 Tyson Cruz
 Matthew McSpadden
 
-last updated 11/25/2018
+last updated 12/03/2018
 
-Module 5.0 adds the search ability for blogs posts.
+Module 6.0 adds the comment ability for blogs posts.
 
 This page shows all the users for an admin.
 */
