4088eab4caf0fbb7dd521dbf34a8761f56fdfe94
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-12-04 22:30:17 -0700

    bootstrap addition

--- blogarchive.php
+++ blogarchive.php
@@ -30,7 +30,7 @@ require_once("nbbc/nbbc.php");
     <meta charset="UTF-8">
     <title>Archive</title>
 
-    <link rel = "Stylesheet" href = "style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body style="background-color: #1abc9c">
 
