baab49241993c5a14675e53d81272ac6d3bc2e85
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-12-04 22:32:11 -0700

    bootstrap addition

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ and changes to a logout button linking to the logout.inc.php.
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
 
