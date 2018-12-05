80b027e9dfb25fe6ae3f27a8f1bb1e9883f10d6d
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-12-04 22:32:46 -0700

    bootstrap addition

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ This is the home page.
   <html>
   <head>
     <title>Blog</title>
-    <link rel='stylesheet' href='style.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
 
