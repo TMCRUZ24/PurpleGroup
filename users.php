731d8cf20c0a800c39c6dc8bb4393c68d4f16cd7
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-12-04 22:33:29 -0700

    bootstrap addition

--- users.php
+++ users.php
@@ -29,7 +29,7 @@ if (!$_SESSION['role'] == 1) {
   <html>
   <head>
     <title>Blog</title>
-    <link rel='stylesheet' href='style.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <div class='wrapper-main'>
